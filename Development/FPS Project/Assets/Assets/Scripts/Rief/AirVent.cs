95c129ae9119aab296ddb733e732b53501415e57
ryuiji <superrief@hotmail.com>
2016-04-05 17:07:25 +0200

    Door en Airvent changes

--- Development/FPS Project/Assets/Assets/Scripts/Rief/AirVent.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/AirVent.cs
@@ -6,15 +6,30 @@ public class AirVent : MonoBehaviour {
     public GameObject airVent;
     public Rigidbody rb;
     private float force = 500f;
+    private float hitRadius = 10f;
+    private float hitDis = 10f;
+    public int roomID;
 
     void Start() {
         rb = airVent.GetComponent<Rigidbody>();
     }
-	
 
-	void OnTriggerEnter(Collider airVentForce) {
-        if(airVentForce.gameObject.tag == "Player") {
-            rb.AddForce(Vector3.up * force);
+
+    void OnTriggerEnter(Collider airVentForce) {
+        if (airVentForce.gameObject.tag == "Player") {
+            AirVentAction(roomID);
         }
-	}
+    }
+
+    void AirVentAction(int RoomID) {
+        rb.AddForce(Vector3.up * force);
+        RaycastHit[] hit = Physics.SphereCastAll(airVent.transform.position, hitRadius, Vector3.forward, hitDis);
+        for (int i = 0; i < hit.Length; i++) {
+            if(hit[i].transform.tag == "Deur") {
+                if (hit[i].transform.GetComponent<Door>().roomID == RoomID && hit[i].transform.GetComponent<Door>().isOpen == false) {
+                    hit[i].transform.GetComponent<Door>().Open();
+                }
+            }
+        }
+    }
 }
--- Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Door.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Door.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class Door : MonoBehaviour {
-
+    public bool isOpen;
+    public int roomID;
 	
 	public void Open () {
         GetComponent<Animator>().SetTrigger("DoorInteraction");
+        isOpen = !isOpen;
 	}
 }
