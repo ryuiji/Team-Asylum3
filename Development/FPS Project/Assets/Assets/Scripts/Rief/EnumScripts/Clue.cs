faa7c996e1bf74a50e34c32385dca8106178568e
Rief Haalboom <superrief@hotmail.com>
2016-04-25 11:40:43 +0200

    ClueManager

--- Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Clue.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Clue.cs
@@ -2,12 +2,27 @@
 using System.Collections;
 
 public class Clue : MonoBehaviour {
-    
+    public enum ClueManager {clueBed, clueBoard}
+    public ClueManager clueManager;
+    public GameObject clueCanvas;
 	void Start () {
 	
 	}
 	
 	public void ClueFound () {
-	
+        clueCanvas.SetActive(true);
+        switch (clueManager) {
+            case ClueManager.clueBed:
+            ClueBed();
+            break;
+            case ClueManager.clueBoard:
+            ClueBoard();
+            break;
+            }
 	}
+    void ClueBed() {
+        }
+    void ClueBoard() {
+
+        }
 }
