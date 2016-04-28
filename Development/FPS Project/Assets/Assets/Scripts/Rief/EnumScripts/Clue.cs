9dd11833b53085467dc297fc68a146fa95a6d698
ryuiji2 <superrief@hotmail.com>
2016-04-28 21:48:44 +0200

    Clues af.

--- Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Clue.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/EnumScripts/Clue.cs
@@ -1,28 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Clue : MonoBehaviour {
-    public enum ClueManager {clueBed, clueBoard}
-    public ClueManager clueManager;
     public GameObject clueCanvas;
-	void Start () {
-	
-	}
+    public int clueNumber;
+    public string[] clues;
+    private int clueMinus = 1;
+    void Start () {
+        clueCanvas.SetActive(false);
+    }
 	
 	public void ClueFound () {
         clueCanvas.SetActive(true);
-        switch (clueManager) {
-            case ClueManager.clueBed:
-            ClueBed();
-            break;
-            case ClueManager.clueBoard:
-            ClueBoard();
-            break;
-            }
-	}
-    void ClueBed() {
-        }
-    void ClueBoard() {
-
-        }
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        clueCanvas.GetComponentInChildren<Text>().text = clues[clueNumber-clueMinus];
+    }
+    public void ExitClues() {
+        clueCanvas.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 }
