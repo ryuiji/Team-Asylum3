740d186cc43ccdf41ff9a242ce802187ee1b310b
Zinedine038 <zsghari@hotmail.com>
2016-05-13 09:19:12 +0200

    Pre hallicination work

--- Development/FPS Project/Assets/Assets/Scripts/Zine/Hallicunation.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/Hallicunation.cs
@@ -4,18 +4,16 @@ using System.Collections;
 
 public class Hallicunation : MonoBehaviour
 {
-    public Sprite[] hallicunation1Images;
-    public AudioClip hallicunation1ScreamSound, hallicunationPantSound;
-    public CanvasGroup hallicunation1CanvasGroup;
+    public AudioClip hallicunationPantSound;
     public AudioSource audioSourceHall1;
     public CanvasGroup blackOut;
-    public PlayerController controller;
+    public QuickieController controller;
     public Gunmanage gunManager;
 
     // Use this for initialization
     void Start()
     {
-        StartCoroutine("HallicunationOne", hallicunation1CanvasGroup);
+        StartCoroutine("BlackOutOne");
     }
 
     // Update is called once per frame
@@ -44,34 +42,10 @@ public class Hallicunation : MonoBehaviour
 
 
 
-    public IEnumerator HallicunationOne(CanvasGroup canvasgroup)
+    public IEnumerator BlackOutOne()
     {
-        for(int i = 0; i<5; i++)
-        {
-            while (canvasgroup.alpha < 0.1f)
-            {
-                FadeImageIn(canvasgroup, 0.25F);
-                yield return new WaitForSeconds(0.01f);
-            }
-            while (canvasgroup.alpha > 0)
-            {
-                FadeImageOut(canvasgroup, 0.25F);
-                yield return new WaitForSeconds(0.01f);
-            }
-        }
-        gunManager.enabled = false;
-        audioSourceHall1.PlayOneShot(hallicunation1ScreamSound);
-        while (canvasgroup.alpha < 1f)
-        {
-            canvasgroup.transform.localScale+=new Vector3(0.5f,0.5f,0.5f);
-            FadeImageIn(canvasgroup, 1);
-            FadeImageIn(blackOut, 2);
-            yield return new WaitForSeconds(0.01f);
-        }
-
-        GetComponent<AudioSource>().Stop();
-        yield return new WaitForSeconds(1f);
-        StartCoroutine("Blackout",canvasgroup);
+        yield return null;
+        StartCoroutine("Blackout",blackOut);
         
     }
 
@@ -79,9 +53,9 @@ public class Hallicunation : MonoBehaviour
     {
         gunManager.enabled=false;
         audioSourceHall1.PlayOneShot(hallicunationPantSound);
-        controller.rotateSpeed = 1f;
-        controller.moveSpeed = 1f;
-        controller.runSpeed = 2f;
+        controller.mouseSpeedX = 60f;
+        controller.mouseSpeedY = 60f;
+        controller.moveSpeed = 2f;
         canvasGroup.alpha = 0;
         while (blackOut.alpha < 1f)
         {
@@ -106,9 +80,9 @@ public class Hallicunation : MonoBehaviour
             FadeImageOut(blackOut, 1);
             yield return new WaitForSeconds(0.01f);
         }
-        controller.rotateSpeed = 4f;
-        controller.moveSpeed = 4f;
-        controller.runSpeed = 8f;
+        controller.mouseSpeedX = 180f;
+        controller.mouseSpeedY = 360f;
+        controller.moveSpeed = 5f;
         gunManager.enabled = true;
     }
 }
