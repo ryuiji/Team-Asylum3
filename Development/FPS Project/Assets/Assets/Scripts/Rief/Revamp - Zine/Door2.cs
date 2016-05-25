62a4bb1375aa5b5c0f01110963c352adfcdd9946
Zinedine038 <zsghari@hotmail.com>
2016-05-25 13:43:31 +0200

    DoorTweak

--- Development/FPS Project/Assets/Assets/Scripts/Rief/Revamp - Zine/Door2.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/Revamp - Zine/Door2.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public class Door2 : MonoBehaviour
 {
+    public bool open;
     public Animator door;
+
+    void Start()
+    {
+        if(open)
+        {
+            UseDoor();
+        }
+    }
 
     public void UseDoor()
     {
