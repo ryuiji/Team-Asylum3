8aaf2653e9f78db46e2d5c4aef70a88dfb826c6e
RobinPC <97026403@st.deltion.nl>
2016-05-26 13:48:32 +0200

    ik moest weer shit commiten van zinedine
    ik moest weer shit commiten van zinedine

--- Development/FPS Project/Assets/Assets/Scripts/Rief/Revamp - Zine/Battery2.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/Revamp - Zine/Battery2.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Battery2 : MonoBehaviour
 {
-    public FlashLight light
+    public FlashLight light;
     public float powerAdded;
     
     public void AddPower()
     {
         light.batteryEnergy+=powerAdded;
+        Destroy(this.gameObject);
     }
 }
