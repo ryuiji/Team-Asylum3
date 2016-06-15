4c8698e4f52bf2f862704aae595e2d6fa988a4ce
RobinPC <97026403@st.deltion.nl>
2016-06-15 17:20:36 +0200

    Ultimate tryhard mode

--- Development/FPS Project/Assets/Assets/Scripts/Rief/LightFlickering.cs
+++ Development/FPS Project/Assets/Assets/Scripts/Rief/LightFlickering.cs
@@ -3,22 +3,39 @@ using System.Collections;
 
 public class LightFlickering : MonoBehaviour {
 
-    public Light lightF;
-    public float minWait;
-    public float maxWait;
+    public Vector2 waitTime;
+    public Vector2 lightMultiplier;
+    public float offTime;
+    private float cooldown;
+    private bool isRunning;
+    private float intensity;
+    private Light light;
 
-	void Start () {
-        lightF.enabled = false;
-        StartCoroutine(LightFlicker());
-
+    void Start () {
+        light = GetComponent<Light>();
+        intensity = light.intensity;
     }
 
+    float SetCooldown () {
+        float random = Random.Range(waitTime.x, waitTime.y);
+        return random;
+    }
 
-    IEnumerator LightFlicker() {
-        lightF.enabled = true;
-        yield return new WaitForSeconds(Random.Range(minWait, maxWait));
-        lightF.enabled = false;
-        yield return new WaitForSeconds(Random.Range(minWait, maxWait));
-        StartCoroutine(LightFlicker());
+    void Update () {
+        if(!isRunning) {
+            cooldown -= Time.deltaTime;
+            if(cooldown <= 0) {
+                StartCoroutine(LightFlicker());
+            }
+        }
+    }
+
+    public IEnumerator LightFlicker () {
+        isRunning = true;
+        light.intensity = intensity * Random.Range(lightMultiplier.x, lightMultiplier.y);
+        yield return new WaitForSeconds(offTime);
+        light.intensity = intensity;
+        cooldown = SetCooldown();
+        isRunning = false;
     }
 }
---
+++ Development/FPS Project/Assets/Assets/Scripts/Robin/HoverManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverManager : MonoBehaviour {
+
+	public Color highlightColor;
+	private Color curColor;
+
+	public void Hover (GameObject obj) {
+		curColor = obj.GetComponent<Text>().color;
+		obj.GetComponent<Text>().color = highlightColor;
+	}
+
+	public void ExitHover (GameObject obj) {
+		obj.GetComponent<Text>().color = curColor;
+	}
+}
