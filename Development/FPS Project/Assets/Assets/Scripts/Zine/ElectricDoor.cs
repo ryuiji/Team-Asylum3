d1c746d3db889424946e97e0db00622e8dbbf0fa
Zinedine038 <zsghari@hotmail.com>
2016-06-29 13:14:41 +0200

    LEGGOOOO
    monstercock commit

---
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/ElectricDoor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricDoor : MonoBehaviour 
+{
+	public ElectricSwitch generator;
+	public bool isOpen;
+	public Animator anim1, anim2;
+	private bool mayOpen =true;
+	public AudioClip doorSound;
+	// Use this for initialization
+	void Start () 
+	{
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			UseDoor();
+		}
+	
+	}
+
+	void UseDoor()
+	{
+		if(generator.active && mayOpen)
+		{
+			if(isOpen)
+			{
+				StartCoroutine(Close());
+			}
+			else if(!isOpen)
+			{
+				StartCoroutine(Open());
+			}
+			isOpen=!isOpen;
+			GetComponent<AudioSource>().PlayOneShot(doorSound);
+		}
+	}
+
+
+	IEnumerator Close()
+	{
+		mayOpen=false;
+		anim1.SetTrigger("Close");
+		anim2.SetTrigger("Close");
+		yield return new WaitForSeconds(2f);
+		mayOpen=true;
+	}
+
+	IEnumerator Open()
+	{
+		mayOpen=false;
+		anim1.SetTrigger("Open");
+		anim2.SetTrigger("Open");
+		yield return new WaitForSeconds(2f);
+		mayOpen=true;
+	}
+}
