ec26d21b7a5cecf8f3346a74d47a868475986f94
Zinedine038 <zsghari@hotmail.com>
2016-06-30 10:23:17 +0200

    Redlight tweaking

---
+++ Development/FPS Project/Assets/Assets/Scripts/Zine/RedFlickerLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedFlickerLight : MonoBehaviour 
+{
+	public Light light;
+	public Material mat;
+	public Color onColor,offColor;
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine("LightFlicker");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	IEnumerator LightFlicker()
+	{
+		light.intensity=0f;
+		mat.SetColor ("_EmissionColor", offColor);	
+		yield return new WaitForSeconds(1f);
+		light.intensity=1.5f;
+		mat.SetColor ("_EmissionColor", onColor);
+		yield return new WaitForSeconds(1f);
+		StartCoroutine("LightFlicker");
+	}
+}
